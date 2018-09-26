7ee43c700dabdaa3783d6d5983623359a37e86c2
Pocholo L. Pantoja <kingbeyondthenorth@gmail.com>
2018-09-26 00:50:39 -0400

    Update file-handling-101_tmp.cpp

--- CPE102L/file-handling-101_tmp.cpp
+++ CPE102L/file-handling-101_tmp.cpp
@@ -1,43 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main(){
-	// Declare file identifier
+	// Declare File Identifier
 	std::ifstream employeeData;
-	// Open the file
+	// Open the File
 	employeeData.open("employeeData.dat");
-	// Verify if the file is open
-	/*
-		if(employeeData.is_open()){ 
-			// output
-		};
-	*/
-	//std::cout << employeeData.is_open();
-	// Declare Three Variables
-	//std::string name;
-	std::string address;
-	float payrate;
+		/* absence of employeeData.dat file leads to a segmentation fault */
+	// Declare Variables
+	std::string address[255];
+	std::string payrate[50];
 	char str[50];
+	char addr[255];
+	char pay[20];
 	int numOfEmp = 0;
 	std::string name[50];
-	
-	// fetch entries
+	std::cout	<< setw(0) << "Name"
+			 	<< setw(35) << "Address"
+				<< setw(50) << "Payrate\n";
+	std::cout << "_________________________________________________________________________________________________________________\n\n";
+	// Fetch Entries
 	while(!employeeData.eof()){
-		employeeData.getline(str, 50, ';');
-		name[numOfEmp] = str;			// save name to array of names
-		std::cout << str << std::endl;
-		employeeData.ignore(255, '\n');
+		employeeData.getline(str,50,';');
+		name[numOfEmp] = str;
+		employeeData.getline(addr,255,';');
+		address[numOfEmp] = addr;
+		employeeData.getline(pay,50,'\n');
+		payrate[numOfEmp] = pay;
+		//std::cout << name[numOfEmp] << setw(60) << address[numOfEmp] << payrate[numOfEmp] <<  endl;
+		printf("%s %60s %40s \n", str, addr, pay);
 		numOfEmp++;
 	}
-	// display names
-	int i = 0;
-	while(i < numOfEmp){
-		std::cout << name[i] << std::endl;
-		i++;
-	}
-	//std::cout << "\nFile Handling";
-	return 0;	
+	return 0;
 }
