1ceb0168960c2fe603debe005add4adab5168cbb
starkfire <kingbeyondthenorth@gmail.com>
2019-01-30 06:58:03 -0500

    full-conversion

--- CPE103L/main.cpp
+++ CPE103L/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>			// to create arrays where you can append new elements
+#include <cstdlib>			// to generate random numbers
 
 using namespace std;
 
@@ -7,122 +9,203 @@ bool firstGame = false;
 
 class Board{
 	public:
-		// 3x3 Board
+		// Fresh State of a 3x3 Board
 		char top[4] = {'0', '0', '0', '\0'};
 		char middle[4] = {'0', '0', '0', '\0'};
 		char bottom[4] = {'0', '0', '0', '\0'};
-		// Array that would store all occupied points
-		char occupied[];
+		// Just an array that will store all occupied points
+		vector<int> occupied;
 
 		// Member Functions
-		void boardReset(void);
-		void winnerStat(void);
-		void statusUpdate(void);
 		bool winCheck(void);
+		void showStatus(void);
 };
 
 class Player{
 	public:
 		string name;
 		bool win;
-		string marker;
-		int score;
-
-		// Member Functions
-		void playerTurn(void);
-		void opponentTurn(void);
+		char marker;
 };
 
 int main(){
 	try{
+		// Class Constructor
+		// Declare objects
 		Board ticTac;
 		Player player1;
 		Player player2;
-		if(firstGame == false){
-			string p1name;
-			cout << "\n" << "INSTRUCTIONS:" << "\n" << "[ 0 1 2 ]" << "\n" << "[ 3 4 5 ]" << "\n" << "[ 6 7 8 ]" << "\n\n";
-			cout << "[?] Player Name: ";
-			cin >> p1name;
-			player1.name = p1name;
-		}
 
+		string p1name;
+		cout << "\n" << "INSTRUCTIONS:" << "\n" << "[ 0 1 2 ]" << "\n" << "[ 3 4 5 ]" << "\n" << "[ 6 7 8 ]" << "\n\n";
+		cout << "[?] Player Name: ";
+		cin >> p1name;
+
+		// By default, all players will start with a win value of false
+		// Player and CPU will differ in terms of their marker (x and o) and name
 		cout << "\n" << "[*] Initializing Player #1" << "\n";
-		player1.score = 0;
+		player1.name = p1name;
 		player1.win = false;
 		player1.marker = 'x';
 
 		cout << "\n" << "[*] Initializing Player #2" << "\n\n";
 		player2.name = "CPU";
-		player2.score = 0;
 		player2.win = false;
 		player2.marker = 'o';
 
+		// Gaming Loop
 		while(true){
 			while(true){
-				player1.playerTurn();
-				break;
+
+				// Player's Turn
+				while(true){
+						int turnInput;
+						cout << "Player #1 Turn: ";
+						cin >> turnInput;
+						// This part is a bit tricky for people who are not used to C++
+						// You need to use a vector library to be able to modify the array
+						// You also have to be very explicit in your conditions
+						if(turnInput >= 0 && turnInput < 9){
+							if(turnInput >= 0 && turnInput <= 2){
+								if(ticTac.top[turnInput] == '0'){
+									ticTac.top[turnInput] = player1.marker;
+									ticTac.occupied.push_back(turnInput);
+									break;
+								}else{
+									cout << "[!] Occupied Point" << endl;
+								}
+							}else if(turnInput >= 3 && turnInput <= 5){
+								turnInput = turnInput - 3;
+								if(ticTac.middle[turnInput] == '0'){
+									ticTac.middle[turnInput] = player1.marker;
+									turnInput = turnInput + 3;
+									ticTac.occupied.push_back(turnInput);
+									break;
+								}else{
+									cout << "[!] Occupied Point" << endl;
+								}
+							}else if(turnInput >= 6 && turnInput <= 8){
+								turnInput = turnInput - 6;
+								if(ticTac.bottom[turnInput] == '0'){
+									ticTac.bottom[turnInput] = player1.marker;
+									turnInput = turnInput + 6;
+									ticTac.occupied.push_back(turnInput);
+									break;
+								}else{
+									cout << "[!] Occupied Point" << endl;
+								}
+							}
+						}else{
+							cout << "[!] Out of Bounds" << endl;
+						}
+				}
+
+				// Check for Winning Patterns
+				if(ticTac.winCheck() == true){
+					player1.win = true;
+					break;
+				}
+
+				//Opponent's Turn
+				while(true){
+					int oppTurn = (rand()%8);
+					if(oppTurn <= 2 && oppTurn >= 0){
+						if(ticTac.top[oppTurn] == '0'){
+							ticTac.top[oppTurn] = player2.marker;
+							ticTac.occupied.push_back(oppTurn);
+							break;
+						}
+					}else if(oppTurn <= 5 && oppTurn >= 3){
+						oppTurn = oppTurn-3;
+						if(ticTac.middle[oppTurn] == '0'){
+							ticTac.middle[oppTurn] = player2.marker;
+							oppTurn = oppTurn+3;
+							ticTac.occupied.push_back(oppTurn);
+							break;
+						}
+					}else if(oppTurn <= 8 && oppTurn >= 6){
+						oppTurn = oppTurn-6;
+						if(ticTac.bottom[oppTurn] == '0'){
+							ticTac.bottom[oppTurn] = player2.marker;
+							oppTurn = oppTurn+6;
+							ticTac.occupied.push_back(oppTurn);
+							break;
+						}
+					}
+				}
+
+				// Check for Winning Patterns
+				if(ticTac.winCheck() == true){
+					player2.win = true;
+					break;
+				}
+
+				// Show Board's Status
+				ticTac.showStatus();
+			}
+			// Declare the Winner
+			if(player1.win == true){
+				cout << "\n" << "WINNER: Player 1" << "\n"; 
+			}else if(player2.win == true){
+				cout << "\n" << "WINNER: CPU" << "\n"; 
+			}else{
+				cout << "\n" << "DRAW" << "\n"; 
 			}
 			break;
 		}
-		/*while(true){
-			while(true){
-				player1.playerTurn();
-				if(ticTac.winCheck() == true){
-					break;
-				}
-				player2.opponentTurn();
-				if(ticTac.winCheck() == true){
-					break;
-				}
-				ticTac.statusUpdate();
-			}
-			ticTac.winnerStat();
-			ticTac.boardReset();
-		}*/
 	}catch(...){
 		cout << "An error occured";
 	}
 }
-void Board::boardReset(void){
-	cout << "Called Board::boardReset";
-	//char top[4] = {'0', '0', '0', '\0'};
-	//char middle[4] = {'0', '0', '0', '\0'};
-	//char bottom[4] = {'0', '0', '0', '\0'};
+
+
+// Shows the Board's Current State
+void Board::showStatus(void){
+	cout << "| " << top[0] << " | " << top[1] << " | " << top[2] << " | " << "\n";
+	cout << "| " << middle[0] << " | " << middle[1] << " | " << middle[2] << " | " << "\n";
+	cout << "| " << bottom[0] << " | " << bottom[1] << " | " << bottom[2] << " | " << "\n";
 }
-void Board::winnerStat(void){
-	cout << "Called Board::winnerStat";
-}
-void Board::statusUpdate(void){
-	cout << "Called Board::statusUpdate";
-}
+
+// Checks for Winning Patterns
 bool Board::winCheck(void){
-	cout << "Called Board::winCheck";
-	return false;
-}
-
-
-void Player::playerTurn(void){
-	while(true){
-		int turnInput;
-		cout << "Player #1 Turn: ";
-		cin >> turnInput;
-		if(turnInput > 8 || turnInput < 0){
-			cout << "Out of Bounds";
-		}else if(turnInput <= 2 && turnInput >= 0){
-			cout << "Top Board Modified";
-			break;
-		}else if(turnInput <= 5 && turnInput >= 3){
-			cout << "Middle Board Modified";
-			break;
-		}else if(turnInput <= 8 && turnInput >= 6){
-			cout << "Bottom Board Modified";
-			break;
-		}else{
-			cout << "Dunno what to fucking do";
-			break;
-		}
+	// Horizontal Patterns
+	if(top[0]=='x' && top[1]=='x' && top[2]=='x'){
+		return true;
+	}else if(top[0]=='o' && top[1]=='o' && top[2]=='o'){
+		return true;
+	}else if(middle[0]=='x' && middle[1]=='x' && middle[2]=='x'){
+		return true;
+	}else if(middle[0]=='o' && middle[1]=='o' && middle[2]=='o'){
+		return true;
+	}else if(bottom[0]=='x' && bottom[1]=='x' && bottom[2]=='x'){
+		return true;
+	}else if(bottom[0]=='o' && bottom[1]=='o' && bottom[2]=='o'){
+		return true;
 	}
-}
-void Player::opponentTurn(void){
-	cout << "Called Player::opponentTurn()";
+	// Vertical Patterns
+	else if(top[0]=='x' && middle[0]=='x' && bottom[0]=='x'){
+		return true;
+	}else if(top[0]=='o' && middle[0]=='o' && bottom[0]=='o'){
+		return true;
+	}else if(top[1]=='x' && middle[1]=='x' && bottom[2]=='x'){
+		return true;
+	}else if(top[1]=='o' && middle[1]=='o' && bottom[2]=='o'){
+		return true;
+	}else if(top[2]=='x' && middle[2]=='x' && bottom[2] == 'x'){
+		return true;
+	}else if(top[2]=='o' && middle[2]=='o' && bottom[2] == 'o'){
+		return true;
+	}
+	// Diagonal Patterns
+	else if(top[0]=='x' && middle[1]=='x' && bottom[2]=='x'){
+		return true;
+	}else if(top[0]=='o' && middle[1]=='o' && bottom[2]=='o'){
+		return true;
+	}else if(top[2]=='x' && middle[1]=='x' && bottom[0]=='x'){
+		return true;
+	}else if(top[2]=='o' && middle[2]=='o' && bottom[2]=='o'){
+		return true;
+	}else{
+		return false;
+	}
 }
